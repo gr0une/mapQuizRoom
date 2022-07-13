35779894b13957b277cca1ad9e455ff99d1f9f86
gr0une <vaillant.g@outlook.com>
2022-07-13 17:59:33 +0200

    changement position

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  });
+app.listen(3000, () => {
+    
+})
+
+// app.listen(process.env.PORT || 5000, () => {
+   
+// });
